27b5e04def6bcbc1732f5fc935e412607d036e76
Sebbito <eric.bortz124@gmail.com>
2022-09-30 19:09:33 +0200

    removed sudo for fish installation for debian

--- fti.sh
+++ fti.sh
@@ -56,8 +56,9 @@ install_fish(){
 		Debian)
 			echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_11/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:3.list
 			curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_11/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
-			sudo apt update
-			sudo apt install fish
+			apt update
+			apt install fish
+			;;
 	esac
 }
 
