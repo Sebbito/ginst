feaa42b6496a165808029db36c2bd54e77b25da4
Sebbito <eric.bortz124@gmail.com>
2022-09-30 19:10:50 +0200

    added -y option for install

--- fti.sh
+++ fti.sh
@@ -57,7 +57,7 @@ install_fish(){
 			echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_11/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:3.list
 			curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_11/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
 			apt update
-			apt install fish
+			apt install -y fish
 			;;
 	esac
 }
