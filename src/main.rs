4339e6321f8aacd97055d7854f6b120b6241fa75
Eric Bortz <e.bortz@netstack.de>
2023-01-15 12:05:24 +0100

    First functioning check for all programms in the json file
    Signed-off-by: Eric Bortz <e.bortz@netstack.de>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::fs;
 use std::process::Command;
 
-#[derive(Debug)]
+use json::JsonValue;
+
+#[derive(Debug, PartialEq)]
 enum Status {
     Installed,
     Missing,
@@ -21,7 +23,6 @@ fn is_installed(programm: String) -> Status {
                     .status()
                     .expect("Failed to execute.");
 
-    println!("{0:#?}", status);
     if status.success() {
         Status::Installed
     } else {
@@ -29,24 +30,53 @@ fn is_installed(programm: String) -> Status {
     }
 }
 
+fn print_status(programms: Vec<Programm>) {
+    for programm in programms.iter() {
+        if programm.status == Status::Missing {
+            println!("[⤫] {}", programm.name)
+        } else {
+            println!("[✓] {}", programm.name)
+        }
+    }
+}
+
+fn generate_prog_vec(json_parsed: JsonValue) -> Vec<Programm> {
+    let mut programms: Vec<Programm> = vec![];
+    let os = get_dist();
+
+    for programm in json_parsed["programms"].members() {
+        let prog = Programm {
+            name: programm["name"].clone().to_string(),
+            install: programm[os.clone()]["install"].clone().to_string(),
+            status: is_installed(programm["name"].clone().to_string()),
+        };
+        programms.push(prog)
+    }
+    programms
+}
+
+fn get_dist() -> String {
+    let output = Command::new("grep")
+                    .args(["^NAME=\".*\"","/etc/os-release"])
+                    .output()
+                    .expect("Failed to get os information");
+
+    let raw = String::from_utf8(output.stdout).expect("Failed to generate string from stoud");
+    raw.replace("\"", "").chars().skip(5).collect()
+}
+
+fn clear() {
+    Command::new("clear").status().expect("Failed to execute");
+}
+
 fn main() {
     let file_contents = fs::read_to_string("programms.json").unwrap();
     let json_parsed = json::parse(&file_contents).expect("Could not parse json file. Maybe you forgot a comma somewhere?");
-    let os = "Fedora";
 
-    // println!("{:#}",json_parsed);
+    let programms = generate_prog_vec(json_parsed);
 
-    let mut programms: Vec<Programm> = vec![];
-    for programm in json_parsed["programms"].members() {
-        let prog = Programm {
-            name: json::stringify(programm["name"].clone()).to_string(),
-            install: json::stringify(programm[os]["install"].clone()).to_string(),
-            status: is_installed(json::stringify(programm["name"].clone()).to_string()),
-        };
+    clear();
 
-        programms.push(prog)
-    }
-
-    println!("{0:#?}", programms);
-    println!("{}",programms[0].name)
+    println!("Programms installed:\n");
+    print_status(programms);
 }
