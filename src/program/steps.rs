4e7f2277aa8d0abbeea965bb94bc587f00c4d7cd
Eric Bortz <e.bortz@netstack.de>
2023-01-17 12:53:01 +0100

    now the execution outputs what's happening
    Signed-off-by: Eric Bortz <e.bortz@netstack.de>

--- src/program/steps.rs
+++ src/program/steps.rs
@@ -13,15 +13,13 @@ pub struct Steps {
 
 impl Steps {
     pub fn execute(&self) {
-        let mut output;
-
         for cmd in self.steps.clone() {
             let step: Vec<&str> = cmd.split(" ").collect();
             let function = step[0].clone();
             let args: Vec<&str>=step.into_iter().skip(1).collect();
-            output = Command::new(function).args(args).output().expect("Could not execute command");
+            let status = Command::new(function).args(args).status().expect("Could not execute command");
 
-            if !output.status.success() {
+            if !status.success() {
                 panic!("Installation instruction didn't finish correctly. Aborting")
             }
         }
