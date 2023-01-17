b3132813b4030de08ac5618d6c0c1fee64780ddf
Eric Bortz <e.bortz@netstack.de>
2023-01-17 18:15:32 +0100

    moved functionality out of utils module to config and installation
    Signed-off-by: Eric Bortz <e.bortz@netstack.de>

--- src/program/util.rs
+++ src/program/util.rs
@@ -1,58 +1,26 @@
-use crate::program::Program;
+use crate::program;
 use json::JsonValue::{self, Null};
-use crate::program::steps::Steps;
-use crate::program::steps;
 
 /*
 * This package provides utility functions for the Program Struct like creating a Vector of
 * programs, building the depenency list and so on.
 */
 
-pub fn build_dependency_list(dependencies: JsonValue) -> Vec<Program> {
+pub fn build_dependency_list(dependencies: JsonValue) -> Vec<program::Program> {
     if dependencies != Null {
-        return as_vec_from_json(dependencies);
+        return program::as_vec_from_json(dependencies);
     } else {
         vec![]
     }
 }
 
-pub fn from_json(json_parsed: &JsonValue) -> Program {
-    let mut prog: Program = Default::default();
-    let mut install_vec: Vec<Steps> = vec![];
-
-    let s_vec = steps::from_json(json_parsed["install"].clone());
-    install_vec.append(&mut s_vec.clone());
-    
-    prog.name = json_parsed["name"].clone().to_string();
-    prog.install = install_vec;
-    prog.status = prog.check();
-
-    if json_parsed["dependencies"] != Null {
-        prog.dependencies = build_dependency_list(json_parsed["dependencies"].clone());
-    } else {
-        prog.dependencies = vec![];
-    }
-    
-    prog
-}
-
-pub fn as_vec_from_json(json_parsed: JsonValue) -> Vec<Program>{
-    let mut programs: Vec<Program> = vec![];
-
-    for program in json_parsed["programs"].members() {
-        programs.push(from_json(program));
-    }
-    
-    return programs;
-}
-
-pub fn install_missing(programs: Vec<Program>) {
+pub fn install_missing(programs: Vec<program::Program>) {
     for prog in programs {
         prog.install();
     }
 }
 
-pub fn count_missing(programs: Vec<Program>) -> u8 {
+pub fn count_missing(programs: Vec<program::Program>) -> u8 {
     let mut counter = 0;
     for p in programs {
         if !p.is_installed() {
