5fd665f5a15090a48f72f9ac54e60e822cffd6c2
Eric Bortz <e.bortz@netstack.de>
2023-01-24 11:53:29 +0100

    fixed the versioning in the bum script
    Signed-off-by: Eric Bortz <e.bortz@netstack.de>

--- bump-version.sh
+++ bump-version.sh
@@ -6,19 +6,19 @@ project_root=$(dirname $script_dir)
 path="$project_root/ginst/Cargo.toml"
 
 if [ $# -eq 1 ]; then
-    if [[ $1 =~ ^v[0-9]\.[0-9]\.[0-9]$ ]]; then
+    if [[ $1 =~ ^[0-9]\.[0-9]\.[0-9]$ ]]; then
         ver=$1
     else
-        echo "Invalid format. Please use vx.x.x as the version number format)."
+        echo "Invalid format. Please use x.x.x as the version number format)."
         exit 1
     fi
 else
-    echo "Please enter a version number (vx.x.x - Format)."
+    echo "Please enter a version number (x.x.x - Format)."
     exit 1
 fi
 
 if [ -f $path ]; then
-    sed -i "s/^version = \"[v0-9\.]*\"$/version = \"$ver\"/" "$path"
+    sed -i "s/^version = \"[0-9\.]*\"$/version = \"$ver\"/" "$path"
 else
     echo "Path $path doesn't exist"
     exit 1
