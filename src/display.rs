fa4459ee09fb73514b0507dc806872fb16ef6e0d
Eric Bortz <e.bortz@netstack.de>
2023-02-05 17:07:47 +0100

    made the display function use generic types that implement the Programmable trait
    Signed-off-by: Eric Bortz <e.bortz@netstack.de>

--- src/display.rs
+++ src/display.rs
@@ -1,9 +1,30 @@
 /// Display interface
 pub mod tui;
 
-use crate::types;
+use crate::types::{self, Programable};
 use std::error::Error;
+
+pub enum UI {
+    TUI,
+    GUI
+}
 
 pub fn run_app(app: &dyn types::Runnable) -> Result<(), Box<dyn Error>> {
     app.run()
+}
+
+pub fn run_ui<T: Programable>(ui_type: UI, items: Vec<T>) {
+    let result = match ui_type {
+        UI::TUI => {
+            let app = tui::App::new(items.clone());
+            run_app(&app)
+        },
+        UI::GUI => {
+            panic!("Not implemented yet")
+        }
+    };
+
+    if let Err(error) = result {
+        panic!("{}", error);
+    }
 }
